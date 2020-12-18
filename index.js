26519a9aef1274d4235bab68af43e355c76841d2
Vijay Singh <SinghVIJ@spar.net>
2020-12-18 17:56:26 +0200

    aded post request with file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let poHeader = { ebeln: 1, werks: "DE01", burks: 1000 };
 let poItem = { ebeln: 1, ebelp: "00010", matnr: "MAT1" };
+
+var fs = require('fs');
+
 // console.log(myPo);
 let http = require("http");
 const express = require('express');
@@ -14,16 +17,41 @@ app.get('/ekko', function(req, res){
     res.type('json');
     res.send(JSON.stringify(poHeader));
 });
-app.get('/ekpo', function(req, res){
+app.get('/ekpo', function(req, res){    
     res.type('json');
-    res.send(JSON.stringify(poItem));
+    //Read a file
+    fs.readFile("C:/Users/singhvij/Desktop/workspace/node/demo1/mynewfile.json", function(err, data) {
+        console.log(data);
+        res.send(JSON.stringify(JSON.parse(data)));
+        return res.end();
+      });
+     ////end of read a file 
+    
 });
 app.post('/ekpo',jsonParser, function(req, res){
     var obj = req.body;
     obj.CreatedBy = "Vijay Singh";
-    res.type("json");
-    res.send(JSON.stringify(obj));
-    res.end();
+    //files
+    fs.readFile("C:/Users/singhvij/Desktop/workspace/node/demo1/mynewfile.json", function(err, data) {
+        // console.log(data);
+        var fileArray = JSON.parse(data);
+        fileArray[fileArray.length] = req.body;
+        res.send(JSON.stringify(fileArray));
+        //over write existing file
+        fs.writeFile("C:/Users/singhvij/Desktop/workspace/node/demo1/mynewfile.json", JSON.stringify(fileArray), function (err) {
+            if (err) throw err;
+            console.log('Replaced!');
+          });
+        return res.end();
+      });
+    // fs.appendFile('./workspace/node/demo1/mynewfile.json', ","+JSON.stringify(obj), function (err) {
+    //     if (err) throw err;
+    //     console.log('Saved!');
+    //   });
+    // ///
+    // res.type("json");
+    // res.send(JSON.stringify(obj));
+    // res.end();
 });
 app.listen(4040);
 
